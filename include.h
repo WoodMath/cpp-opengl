d9a4eadd9556d04081fc6655ad83a1a10a642d38
Jeff Wood <Jeff@Jeffs-MacBook-Pro.local>
2015-05-11 18:24:45 -0500

    Added 'include.h'

---
+++ include.h
@@ -0,0 +1,18 @@
+//#include "GL/glew.h"
+
+#ifdef __linux__
+#include <GL/glut.h>
+#endif
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#endif
+
+#ifdef _WIN32
+#include "glut.h"
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
